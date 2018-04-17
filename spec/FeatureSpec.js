b79fa301d8be958f6b220ef1eae7a933caee3bc6
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-17 14:22:08 +0100

    capacity added & plane not allowed to land when full

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -3,41 +3,55 @@ describe('Airport', function() {
   var airport;
 
   beforeEach(function() {
-   airport = new Airport();
+   // airport = new Airport();
    plane = new Plane();
    });
 
    describe('has the plane after landing', function() {
-     it('has the plane', function() {
-       airport.land(plane, 'sunny')
-       expect(airport.hangar).toContain(plane);
+      it('has the plane', function() {
+          var airport1 = new Airport()
+          airport1.land(plane, 'sunny')
+       expect(airport1.hangar).toContain(plane);
      });
    });
 
    describe('does not hava the plane after taking off', function() {
      it('has no plane', function() {
-       airport.take_off(plane, 'sunny')
-       expect(airport.hangar).not.toContain(plane);
+        var airport2 = new Airport()
+        airport2.take_off(plane, 'sunny')
+       expect(airport2.hangar).not.toContain(plane);
      });
    });
 
    describe('when stormy, plane is not allowed to', function() {
      it('land', function() {
-      var airport = function(){
-        airport = new Airport()
-        airport.land(plane, 'stormy')
+      var airport3 = function(){
+        airport3 = new Airport()
+        airport3.land(plane, 'stormy')
       }
-       expect(airport).toThrowError('Plane is not allowed to land when stormy');
+       expect(airport3).toThrowError('Plane is not allowed to land when stormy');
      });
 
      it('take off', function() {
-      var airport = function(){
-        airport = new Airport()
-        airport.take_off(plane, 'stormy')
+      var airport4 = function(){
+        airport4 = new Airport()
+        airport4.take_off(plane, 'stormy')
       }
-       expect(airport).toThrowError('Plane is not allowed to take off when stormy');
+       expect(airport4).toThrowError('Plane is not allowed to take off when stormy');
      });
+   });
 
+   describe('raises an error when', function() {
+     it('airport is full', function() {
+       var airport5 = function(){
+        airport5 = new Airport(3)
+        var times = 4;
+        for(var i=0; i < times; i++){
+          airport5.land(plane, 'sunny');
+        }
+       }
+       expect(airport5).toThrowError('Airport is full');
+     });
    });
 
 
--- src/airport.js
+++ src/airport.js
@@ -2,17 +2,31 @@ Array.prototype.random = function(){
   return this[Math.floor(Math.random() * this.length)]
 };
 
-var Airport = function (){
-  this.weather = ["stormy", "sunny", "sunny", "sunny"].random();
-  this.hangar = []
+var DEFAULT_CAPACITY = function(){
+  return 50
 };
 
 var Plane = function (){};
 
+var Airport = function (capacity = DEFAULT_CAPACITY){
+  this.weather = ["stormy", "sunny", "sunny", "sunny"].random();
+  this.hangar = []
+  this.capacity = capacity
+};
+
+Airport.prototype._isfull = function(length = this.hangar.length, capacity = this.capacity){
+  if(length === capacity){
+    return ture;
+  }
+   return false;
+};
 
 Airport.prototype.land = function(plane, weather = this.weather) {
   if (weather === 'stormy'){
     throw new TypeError("Plane is not allowed to land when stormy")
+  }
+  if (this.hangar.length === this.capacity ){
+    throw new TypeError('Airport is full')
   }
   else{
     this.hangar.push(plane)
